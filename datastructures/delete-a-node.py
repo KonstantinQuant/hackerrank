fdd8b0130897dd77955dec1e2ed5d9ea1b6413fe
Konstantin Kuchenmeister <88515336+KonstantinQuant@users.noreply.github.com>
2021-09-11 17:25:54 +0200

    Create delete-a-node.py

---
+++ datastructures/delete-a-node.py
@@ -0,0 +1,12 @@
+def deleteNode(llist, position):
+    # Write your code here
+    if position == 0:
+        llist = llist.next
+    else:
+        node = llist
+        for _ in range(position-1):
+            node = node.next
+        
+        node.next = node.next.next
+    
+    return llist
