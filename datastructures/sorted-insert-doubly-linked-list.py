c5242a04b1343cd12368dbdc170d9d2a8b080514
Konstantin Kuchenmeister <88515336+KonstantinQuant@users.noreply.github.com>
2021-09-12 18:11:46 +0200

    Create sorted-insert-doubly-linked-list.py

---
+++ datastructures/sorted-insert-doubly-linked-list.py
@@ -0,0 +1,16 @@
+# only 3/7 testcases passing here?...
+def sortedInsert(llist, data):
+    head = llist
+    new = DoublyLinkedListNode(data)
+    
+    if head is None:
+        head = new
+    
+    while llist.next is not None and llist.next.data <= data:
+        llist = llist.next
+        
+    new.next = llist.next
+    new.prev = llist
+    llist.next = new
+
+    return head
